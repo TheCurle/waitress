3bc772080d2e4140b2a52f16d845622c6d80608e
Curle <curle@gemwire.uk>
2021-08-30 19:17:00 +0100

    Add comments on Config.java

--- src/main/java/uk/gemwire/waitress/config/Config.java
+++ src/main/java/uk/gemwire/waitress/config/Config.java
@@ -8,8 +8,16 @@ import java.util.Map;
  */
 public final class Config {
 
+    // The port number that the web server listens on.
     public static int LISTEN_PORT = 0;
 
+    /**
+     * Set the config values in the above fields.
+     * Allows these fields to be used to access the below configuration values from any point in the program,
+     * without propagating the configuration map to every function that reads it.
+     *
+     * @param args The map of config name -> value to be used to set the fields.
+     */
     public static void set(Map<String,String> args) {
         LISTEN_PORT = Integer.parseInt(args.get("listen_port"));
     }
